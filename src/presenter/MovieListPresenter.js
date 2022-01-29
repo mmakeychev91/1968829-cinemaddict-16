205f86044493f9eb57d521f3da6503ddd468d11b
Михаил Макейчев <mihailmakejcev@MacBook-Air-Mihail.local>
2022-01-29 18:29:06 +0300

    processing

--- src/presenter/MovieListPresenter.js
+++ src/presenter/MovieListPresenter.js
@@ -3,8 +3,7 @@ import DetailInfoPopup from '../view/detail-info-popup.js';
 import ShowMoreButton from '../view/show-more-button.js';
 import {
   render,
-  RenderPosition,
-  remove
+  RenderPosition
 } from './../utils/render';
 
 const FILM_CARDS_AMOUNT_PER_STEP = 5;
@@ -13,13 +12,10 @@ const body = document.querySelector('body');
 export default class MovieListPresenter {
   #filmCardsContainer = null;
 
-  #filmCard = (callback) => {
-    new FilmCard(callback);
-  };
+  #filmCard = (callback) => new FilmCard(callback);
 
-  #detailInfoPopup = (callback) => {
-    new DetailInfoPopup(callback);
-  };
+  #detailInfoPopup = (callback) => new DetailInfoPopup(callback);
+
   #showMoreButton = new ShowMoreButton();
 
   #filmCards = [];
@@ -41,35 +37,38 @@ export default class MovieListPresenter {
 
   }
 
-  #renderPopup = () => {
-    render(this.#filmCardsContainer, this.#detailInfoPopup, RenderPosition.BEFOREEND);
+  #renderPopup = (detailInfoCardPopup) => {
+    render(body, detailInfoCardPopup, RenderPosition.BEFOREEND);
+    body.classList.add('hide-overflow');
   }
 
-  #clickFilmCard = () => {
-    this.#filmCard.setOpenClickHandler(() => {
-      this.#renderPopup;
-      body.classList.add('hide-overflow');
-      this.#escOpenPopup;
-      this.#clickClosePopup;
-    });
+  #closePopup = () => {
+    this.#detailInfoPopup.removeElement();
+    body.classList.remove('hide-overflow');
+  };
 
-  }
-  //TODO: Ну правда я бы вынес у отдельную функцию закоытие попапа. Потому что у тебя по клику и по кнопке одно и тоже должно происходить
   #clickClosePopup = () => {
     this.#detailInfoPopup.setCloseClickHandler(() => {
-      this.#detailInfoPopup.removeElement();
-      body.classList.remove('hide-overflow');
+      this.#closePopup();
     });
 
   }
+
   #escOpenPopup = (evt) => {
     if (evt.key === 'Escape' || evt.key === 'Esc') {
       evt.preventDefault();
-      this.#detailInfoPopup.removeElement();
-      body.classList.remove('hide-overflow');
-      document.removeEventListener('keydown', this.#escOpenPopup);
-    };
+      this.#closePopup();
+      document.removeEventListener('keydown', this.#escOpenPopup());
+    }
   };
+
+  #clickFilmCard = (filmCard, detailInfoCardPopup) => {
+    filmCard.setOpenClickHandler(() => {
+      this.#renderPopup(detailInfoCardPopup);
+      document.addEventListener('keydown', this.#escOpenPopup());
+    });
+
+  }
 
   #clickShowMoreButton = () => {
     this.#showMoreButton.setShowMoreClickHandler(() => {
@@ -92,7 +91,9 @@ export default class MovieListPresenter {
   #renderCertainQuantityCards = () => {
     //метод отрисовки n количества карточек за раз
     for (let i = 0; i < Math.min(this.#filmCards.length, FILM_CARDS_AMOUNT_PER_STEP); i++) {
-      this.#createFilmCards(this.#filmCard(this.#filmCards[i]));
+      const currentFilmCard = this.#filmCard(this.#filmCards[i]);
+      const currentPopup = this.#detailInfoPopup(this.#filmCards[i]);
+      this.#createFilmCards(currentFilmCard, currentPopup);
       //this.#detailInfoPopup(this.#filmCards[i]);
     }
   }
@@ -107,33 +108,11 @@ export default class MovieListPresenter {
 
   #createFilmCards = (filmCard, detailInfoCardPopup) => {
     render(this.#filmCardsContainer, filmCard, RenderPosition.BEFOREEND);
-    /*
-    //#clickFilmCard
-    filmCard.setOpenClickHandler(() => {
-      //#renderPopup
-      render(body, detailInfoCardPopup, RenderPosition.BEFOREEND);
-      body.classList.add('hide-overflow');
-      //#escOpenPopup 
-      const onEscKeyDown = (evt) => {
-        if (evt.key === 'Escape' || evt.key === 'Esc') {
-          evt.preventDefault();
-          remove(detailInfoCardPopup);
-          body.classList.remove('hide-overflow');
-          document.removeEventListener('keydown', onEscKeyDown);
-        }
-      };
-      document.addEventListener('keydown', onEscKeyDown);
-      //#clickClosePopup
-      detailInfoCardPopup.setCloseClickHandler(() => {
-        remove(detailInfoCardPopup);
-        body.classList.remove('hide-overflow');
-      });
-    });*/
+    this.#clickFilmCard(filmCard, detailInfoCardPopup);
   };
 
   #renderFilmCards = () => {
     this.#renderCertainQuantityCards();
-
   };
 
-};
+}
